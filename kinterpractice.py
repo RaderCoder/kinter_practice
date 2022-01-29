9709f2fb4c6e49045713a99a048fec7d712de1cd
Radee Al Mahmood <radee.mahmood@northsouth.edu>
2022-01-29 08:46:35 +0600

    This should have been the initial commit

--- kinterpractice.py
+++ kinterpractice.py
@@ -2,4 +2,9 @@ from tkinter import *
 
 root = Tk()
 
+#Creating a Label Widget
 myLabel = Label(root, text="That doesn't work for me Brother")
+#Shoving it onto the screen
+myLabel.pack()
+
+root.mainloop()
